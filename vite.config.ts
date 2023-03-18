7bd7094eca74e4298ee3e4f991b7e52fb1ee7d65
kent <526690390@qq.com>
2023-03-19 00:56:05 +0800

    chore: replenish

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
+  base: "./",
   server: {
     host: "0.0.0.0",
-    port: 8000,
+    port: 3000,
   },
 });
