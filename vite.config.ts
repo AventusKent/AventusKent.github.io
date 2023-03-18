5aa367bc63d35545aee8efdace65e490e3e9971a
kent <526690390@qq.com>
2023-03-19 01:05:25 +0800

    chore: change

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+// Utilities
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "./",
+  define: { "process.env": {} },
   server: {
-    host: "0.0.0.0",
     port: 3000,
+    open: true,
+    cors: true,
   },
+  base: "./",
 });
