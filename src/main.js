05f65a0cf601bbfac0e6d2f151b1b75d6f94c39d
kent <526690390@qq.com>
2023-03-23 16:54:18 +0800

    change dist

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import './css.css';
-import 'github-markdown-css';
-import router from '@/router';
-import App from './App.vue';
-
-createApp(App).use(router).mount('#app');
